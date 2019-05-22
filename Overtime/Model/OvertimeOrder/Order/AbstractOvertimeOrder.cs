00c4861889dc38cb7b5ea7a6d6dd011734e17701
zheng <zheng.xiangfei@rococo.co.jp>
2019-05-22 13:53:14 +0800

    add overtimeitem function

--- Overtime/Model/OvertimeOrder/Order/AbstractOvertimeOrder.cs
+++ Overtime/Model/OvertimeOrder/Order/AbstractOvertimeOrder.cs
@@ -1,15 +1,17 @@
 ﻿using Common.Model;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Overtime.Model.OvertimeOrder.Order
 {
-    public abstract  class AbstractOvertimeOrder : Entity<Guid>
+    public abstract class AbstractOvertimeOrder : Entity<Guid>
     {
         protected Guid StaffId { get; set; }
         protected OvertimeOrderMonth Month { get; set; }
         protected OvertimeOrderStatus Status { get; set; }
+
+        protected void addOvertimeItem()
+        {
+        }
 
     }
 }
---
+++ Overtime/Model/OvertimeOrder/OrderItem/OvertimeNote.cs
@@ -0,0 +1,15 @@
+﻿using Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overtime.Model.OvertimeOrder.OrderItem
+{
+    public class OvertimeNote : ValueObject<OvertimeNote>
+    {
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
